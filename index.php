818280f4ae3e3e1aa6cd30d73e6a2275051d2555
jeronimo-programador <70165397+jeronimo-programador@users.noreply.github.com>
2020-08-24 12:46:19 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,178 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<style type="text/css">
+
+body{
+	background: url(fondo.jpg);
+	background-attachment: fixed;
+	background-size: cover;
+	background-repeat: none;
+}
+.us, .contenedor{
+    width: 300%;
+}
+.us, .contenedor, .prov{
+    background-color: rgba(0,0,0,.7);
+    color: white;
+    font-family: 'Maven Pro';
+    width: 50%;
+    padding: 20px;
+    border-radius: 10px;
+}
+
+
+
+input[type="text"]{
+	border: 0px;
+	padding: 5px;
+	border-radius: 5px;
+	margin-bottom: 5px;
+	border-bottom: 5px solid #173D3A;
+	margin-top: 7px;
+}
+
+h1{
+	font-family: Arial;
+	font-size: 30px;
+}
+
+
+
+
+.header{
+	background-color: #173D3A;
+	color: white;
+	border-radius: 10px;
+}
+
+td{
+	padding: 10px;
+}
+
+tr{
+	border-bottom:4px solid #173D3A;
+}
+
+tr:hover{
+	background-color: #6E91CB;
+	color: white;
+	transition: .5s;
+}
+
+input[type="submit"], input[type="reset"]{
+  border: 0px solid #797878; 
+  padding: 10px;
+  border-radius: 10px;
+  background-color: silver;
+
+}
+
+	
+
+</style>
+<style type="text/css">
+    
+    *{
+        padding: 0;
+        margin: 0;
+    }
+    p{
+        margin-bottom: 20px;
+    }
+    header{
+        padding-left: 350px;
+        background: rgba(0,0,0,0.9);
+        width: 100%;
+        position: fixed;
+        z-index: 100;
+    }
+    nav{
+        float:left;
+    }
+
+    nav ul{
+        list-style: none;
+        overflow: hidden;
+    }
+    nav ul li{
+        float: left;
+        font-family: arial,sans-serief;
+        font-size: 16px;
+    }
+    nav ul li a{
+        display: block;
+        padding: 20px;
+        color: #fff;
+        text-decoration: none;
+
+    }
+    nav ul li:hover{
+        background: #3ead47;
+    }
+    .contenido{
+        padding-top: 80px;
+    }
+    .wrapper{
+        width: 80px%;
+        margin: auto;
+        overflow: hidden;
+    }
+</style>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Tutoriales</a></li>
+                <li><a href="#">Cursos</a></li>
+                <li><a href="#">Preguntame algo</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="contenido wrapper">
+        <p>
+
+	<center>
+	
+	 <br>
+    
+    <div class="prov">
+    	<h1>Un sueño que se lleva en la sangre</h1>
+        <img src="logo.png">
+
+    	
+    		
+    	
+    </div> <br>
+
+
+     <div class="contenedor">
+        <h1>Conocenos</h1>
+        <img src="afiche.png" width="300" height="450">
+        <img src="afiche2.png" width="300">
+        <form action="producto.php" method="POST">
+   
+        </form>
+    </div><br>
+      <div class="contenedor">
+        <h1>Tu Buscador</h1>
+        <form action="" method="POST">
+            <label for="" class="tx"></label><br><input type="text" name="nombre_p" placeholder="Digite.."><br>
+            <input type="submit" value="Buscar">
+            
+        </form>
+    </div>
+
+</center>
+</p>
+
+   </section>
+</body>
+</html>
